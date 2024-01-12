502fe89bf1bd517c798de1c0c388ba49b79f4b48
alvaromd99 <117214600+alvaromd99@users.noreply.github.com>
2024-01-12 16:25:41 +0100

    make the hover effect on the cards

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,4 +1,5 @@
 import './card.css'
+import ArrowIcon from '../../assets/topRightArrow.svg'
 
 interface CardProps {
 	name: string
@@ -9,6 +10,9 @@ export const Card = ({ name, image }: CardProps) => {
 	return (
 		<div className='card'>
 			<p>{name}</p>
+			<span>
+				<img src={ArrowIcon} alt='Arrow' />
+			</span>
 			<img src={image} alt={`${name} project image.`} />
 		</div>
 	)
