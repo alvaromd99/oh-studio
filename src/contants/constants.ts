4345b00a0a9bfad5ecb80f9c12e4922aee32de34
alvaromd99 <117214600+alvaromd99@users.noreply.github.com>
2024-01-16 12:00:24 +0100

    add constants about the experience and the clients for the profile page

--- src/contants/constants.ts
+++ src/contants/constants.ts
@@ -24,3 +24,94 @@ export const projectDetails = [
 			'https://framerusercontent.com/images/BAXywubxN8sAhfJg3AgGgFI.jpg?scale-down-to=1024',
 	},
 ]
+
+export const workExperienceDetails = [
+	{
+		id: 1,
+		timeWorking: '2017 - Present',
+		place: 'OH.STUDIO',
+		position: 'Freelance Designer',
+		type: 'Director',
+	},
+	{
+		id: 2,
+		timeWorking: '2021 - Present',
+		place: 'Friendly Studio',
+		position: 'Lead Product Designer',
+		type: 'Freelance',
+	},
+	{
+		id: 3,
+		timeWorking: '2022',
+		place: 'R/GA',
+		position: 'Senior Visual Designer',
+		type: 'Freelance',
+	},
+	{
+		id: 4,
+		timeWorking: '2021',
+		place: 'R/GA',
+		position: 'Senior Visual Designer',
+		type: 'Freelance',
+	},
+	{
+		id: 5,
+		timeWorking: '2021',
+		place: 'AKQA',
+		position: 'Senior Designer',
+		type: 'Freelance',
+	},
+	{
+		id: 6,
+		timeWorking: '2020',
+		place: 'AKQA',
+		position: 'Senior Designer',
+		type: 'Freelance',
+	},
+	{
+		id: 7,
+		timeWorking: '2019',
+		place: 'UI Centric',
+		position: 'Senior Product Designer',
+		type: 'Freelance',
+	},
+	{
+		id: 8,
+		timeWorking: '2017',
+		place: 'Agency TK',
+		position: 'Senior Digital Designer',
+		type: 'Permanent',
+	},
+	{
+		id: 9,
+		timeWorking: '2012',
+		place: 'Agency TK',
+		position: 'Digital Designer',
+		type: 'Permanent',
+	},
+	{
+		id: 10,
+		timeWorking: '2009',
+		place: 'Next Level',
+		position: 'Digital Designer',
+		type: 'Permanent',
+	},
+	{
+		id: 11,
+		timeWorking: '2008',
+		place: 'Next Level',
+		position: 'Junior Digital Designer',
+		type: 'Permanent',
+	},
+]
+
+export const clientDetails = [
+	'https://framerusercontent.com/images/nXLdc1LsU15dk6AODEpMgByLDw.png?scale-down-to=512',
+	'https://framerusercontent.com/images/roAzNhDBr8shtI8EFOALn1BTMO0.png?scale-down-to=512',
+	'https://framerusercontent.com/images/vzRTkTnoLuK5zvS2serU2qCb4.png?scale-down-to=512',
+	'https://framerusercontent.com/images/DFu6Bri5cuYIVyEC6OTxtuZC6w.png?scale-down-to=512',
+	'https://framerusercontent.com/images/IDRNrtKWyB6T6o3dY4mUDqueC0.png?scale-down-to=512',
+	'https://framerusercontent.com/images/X5Lg2eapOCFOhSpAFsZNTW9BlY.png?scale-down-to=512',
+	'https://framerusercontent.com/images/PCQIe7uLIcRFBG5bKFWCX3niBek.png?scale-down-to=512',
+	'https://framerusercontent.com/images/8w3YBn3xA3P0u5w43PGhPo1jpYA.png?scale-down-to=512',
+]
--- src/pages/profile/Profile.tsx
+++ src/pages/profile/Profile.tsx
@@ -5,7 +5,17 @@ export const Profile = () => {
 	return (
 		<div className='page profile-page'>
 			<Header />
-			Profile
+			<h1>Hi 👋🏼 I'm Oli.</h1>
+			<div className='profile-images-cont'>
+				<img
+					src='https://framerusercontent.com/images/wJINOk3BFAbQRx6ZySXfbltPxU.jpg?scale-down-to=1024'
+					alt='Profile image'
+				/>
+				<img
+					src='https://framerusercontent.com/images/O3Foxt0XkzTsjGc74Ap0Hrxtz4c.jpg?scale-down-to=1024'
+					alt='Profile image 2'
+				/>
+			</div>
 		</div>
 	)
 }
