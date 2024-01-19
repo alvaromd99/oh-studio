d37f0982413a6d64eac7b29bc654961b3d5844e2
alvaromd99 <117214600+alvaromd99@users.noreply.github.com>
2024-01-19 11:14:57 +0100

    use link instead of setlocation

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,34 +1,33 @@
-import { useLocation } from 'wouter'
+import { Link, useLocation } from 'wouter'
 import './header.css'
 
 export default function Header() {
-	const [location, setLocation] = useLocation()
-
-	const handleClick = (to: string) => {
-		setLocation(to)
-	}
+	const [location] = useLocation()
 
 	return (
 		<div className='switch'>
-			<div
-				className={`option home-opt ${location === '/' ? 'selected' : ''}`}
-				onClick={() => handleClick('/')}>
-				Home
-			</div>
-			<div
-				className={`option profile-opt ${
-					location === '/profile' ? 'selected' : ''
-				}`}
-				onClick={() => handleClick('/profile')}>
-				Profile
-			</div>
-			<div
-				className={`option profile-opt ${
-					location === '/contact' ? 'selected' : ''
-				}`}
-				onClick={() => handleClick('/contact')}>
-				Contact
-			</div>
+			<Link to='/'>
+				<div
+					className={`option home-opt ${location === '/' ? 'selected' : ''}`}>
+					Home
+				</div>
+			</Link>
+			<Link to='/profile'>
+				<div
+					className={`option profile-opt ${
+						location === '/profile' ? 'selected' : ''
+					}`}>
+					Profile
+				</div>
+			</Link>
+			<Link to='/contact'>
+				<div
+					className={`option profile-opt ${
+						location === '/contact' ? 'selected' : ''
+					}`}>
+					Contact
+				</div>
+			</Link>
 		</div>
 	)
 }
