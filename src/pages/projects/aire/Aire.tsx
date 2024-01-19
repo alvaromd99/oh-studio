af33e55942901cb2065d461d8a59c0499dec5309
alvaromd99 <117214600+alvaromd99@users.noreply.github.com>
2024-01-19 13:17:04 +0100

    add more images to aire

--- src/pages/projects/aire/Aire.tsx
+++ src/pages/projects/aire/Aire.tsx
@@ -38,6 +38,23 @@ export default function Aire() {
 						'I worked with Aire to develop a brand that firmly positions them as a premium lifestyle brand. The name Aire was inspired by the companies conception in Aire Street Workshops, in addition to aligning well with how the product makes you feel. The wordmark has been designed to convey the feeling of ease and simplicity whilst encapsulating a premium feel. Aire believes in the power of CBD to help people manage the day to day a little better — so I developed a strap-line which embodied that message. A better way to deal with every day.'
 					}
 				/>
+				<img
+					src={
+						'https://framerusercontent.com/images/1ZNxQVbQdraZjjmKyBwxrioAwvc.jpg?scale-down-to=2048'
+					}
+					alt='Aire image 4'
+					className='big-img'
+				/>
+				<div className='double-img'>
+					<img
+						src='https://framerusercontent.com/images/wQuOrcu37DXYfaXafEJ6y6Uo.jpg?scale-down-to=2048'
+						alt='Aire image 5'
+					/>
+					<img
+						src='https://framerusercontent.com/images/kfr022WaJ8YJUyZFmdwCB3pXhs.jpg?scale-down-to=2048'
+						alt='Aire image 6'
+					/>
+				</div>
 			</main>
 		</div>
 	)
